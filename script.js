3831d6846f7e365a44bf44af9cc7ae6c8b3284cb
Sulleh1992 <ben.sullivan1992@gmail.com>
2024-02-29 13:16:04 +0000

    v.1- start, end, corridor blocks
    Significant changes to base code to simplify logic and how functions interact with each other.
    
    Added corridor blocks to create difficulty.
    
    Stored goals-to-be-done and goals-reached within arrays to allow for a logical end point to the game.
    
    Adjusted arrow button logic to allow changes based upon the presence of blocks.
    
    Multiple aesthetic changes.
    
    Implemented pop-ups with buttons to start/restart at start and end of game respectively.

--- script.js
+++ script.js
@@ -1,171 +1,265 @@
-//Create arrays to store done and to-be-done goals.
-const ToDo = ['sp0', 'sp1', 'sp2', 'sp3', 'sp4', 'sp5', 'sp6', 'sp7', 'sp8'];
-const Done = [];
-
-// Assign buttons to constants.
-const upButton = document.getElementById("up"),
-      downButton = document.getElementById("down"),
-      leftButton = document.getElementById("left"),
-      rightButton = document.getElementById("right");
-
-// Event listener for button clicks
-document.addEventListener("DOMContentLoaded", () => {
-    upButton.addEventListener("click", () => {
-        goUp();
-        refreshTiles(player.xCartCoord, player.yCartCoord);
-    });
-    downButton.addEventListener("click", () => {
-        goDown();
-        refreshTiles(player.xCartCoord, player.yCartCoord);
-    });
-    leftButton.addEventListener("click", () => {
-        goLeft();
-        refreshTiles(player.xCartCoord, player.yCartCoord);
-    });
-    rightButton.addEventListener("click", () => {
-        goRight();
-        refreshTiles(player.xCartCoord, player.yCartCoord);
-    });
-});
-
-// Constants for converting pixel coordinates to cartesian coordinates
-const xPxToCart = (x) => (x - 8) / 64,
-      xCartToPx = (x) => (64 * x) + 8,
-      yPxToCart = (y) => (y - 28) / 64,
-      yCartToPx = (y) => (y * 64) + 28;
-
-// Functions to obtain coordinates
-const getFirstX = () => parseInt(window.getComputedStyle(player).getPropertyValue("left")),
-      getFirstY = () => parseInt(window.getComputedStyle(player).getPropertyValue("top")),
-      getX = () => parseInt(window.getComputedStyle(player).getPropertyValue("left")),
-      getY = () => parseInt(window.getComputedStyle(player).getPropertyValue("top")),
-      getCoords = (x, y) => `${xPxToCart(x)},${yPxToCart(y)}`,
-      getTile = (cartCoords) => {
-          const SpacesCoords = ['0,0', '1,0', '2,0', '0,1', '1,1', '2,1', '0,2', '1,2', '2,2'];
-          const index = SpacesCoords.indexOf(cartCoords);
-          return `sp${index}`;
+// Arrays to store goal-tiles to be reached (ToDo) and goal-tiles reached (Done)
+const ToDo = ['sp0', 'sp1', 'sp2', 'sp3', 'sp4', 'sp5', 'sp6', 'sp7', 'sp8'],
+      Done = [],
+      // Object to store possible moves for each tile
+      PossibleMovesByTile = {
+          sp0: [['right', 'down'], ['cor0', 'cor2']],
+          sp1: [['left', 'right', 'down'], ['cor0', 'cor1', 'cor3']],
+          sp2: [['left', 'down'], ['cor1', 'cor4']],
+          sp3: [['up', 'right', 'down'], ['cor2', 'cor5', 'cor7']],
+          sp4: [['up', 'left', 'right', 'down'], ['cor3', 'cor5', 'cor6', 'cor8']],
+          sp5: [['up', 'left', 'down'], ['cor4', 'cor6', 'cor9']],
+          sp6: [['up', 'right'], ['cor7', 'cor10']],
+          sp7: [['up', 'left', 'right'], ['cor8', 'cor10', 'cor11']],
+          sp8: [['up', 'left'], ['cor9', 'cor11']]
       };
 
-// Function to update tiles
-const updateTiles = (inputId) => {
-    const Tiles = document.getElementsByClassName("occupied");
-    if (Tiles.length>0) {
-        Tiles[0].classList.remove("occupied");
-    }
-    const currentTile = document.getElementById(inputId);
+// Object to store buttons and methods to disable/enable them
+const Buttons = {
+    dirUp: document.querySelector("#up"),
+    dirLeft: document.querySelector("#left"),
+    dirRight: document.querySelector("#right"),
+    dirDown: document.querySelector("#down"),
+    upDisable: () => { Buttons.dirUp.disabled = true; },
+    leftDisable: () => { Buttons.dirLeft.disabled = true; },
+    rightDisable: () => { Buttons.dirRight.disabled = true; },
+    downDisable: () => { Buttons.dirDown.disabled = true; },
+    disableAll: () => { Buttons.upDisable(); Buttons.leftDisable(); Buttons.rightDisable(); Buttons.downDisable(); },
+    resetAll: () => { Buttons.dirUp.disabled = false; Buttons.dirLeft.disabled = false; Buttons.dirRight.disabled = false; Buttons.dirDown.disabled = false; }
+};
+
+// Function to set event listeners for directional button clicks
+const setButtonListeners = () => {
+    ['Up', 'Left', 'Right', 'Down'].forEach(direction => {
+        if (Buttons[`dir${direction}`]) {
+            Buttons[`dir${direction}`].addEventListener("click", () => {
+                movePlayer(direction);
+                refreshTiles(player.xCartCoord, player.yCartCoord);
+            });
+        }
+    });
+};
+
+// Functions for converting pixel coordinates to cartesian coordinates and RNG
+const xPxToCart = x => (x - 8) / 64,
+      xCartToPx = x => (64 * x) + 8,
+      yPxToCart = y => (y - 28) / 64,
+      yCartToPx = y => (y * 64) + 28,
+      rand = max => Math.floor(Math.random() * max);
+
+// Functions to get player location variables
+const getX = () => parseInt(window.getComputedStyle(player).getPropertyValue("left")),
+      getY = () => parseInt(window.getComputedStyle(player).getPropertyValue("top")),
+      // Function to get tile based on cartesian coordinates
+      getTile = cartCoords => {
+          const SpacesCoords = ['0,0', '1,0', '2,0', '0,1', '1,1', '2,1', '0,2', '1,2', '2,2'];
+          return `sp${SpacesCoords.indexOf(cartCoords)}`;
+      };
+
+// Function to update tile styles
+const updateOccupiedTile = currentTileId => {
+    const currentTile = document.getElementById(currentTileId);
     currentTile.classList.add("occupied");
-    player.occupiedTile = currentTile;
 };
 
-// Function to refresh tiles and set button states
+// Function to refresh tiles
 const refreshTiles = (x, y) => {
-    setButtons(x, y);
-    const coords = `${x},${y}`;
-    player.cartCoords = coords;
-    const tileId = getTile(coords);
-    updateTiles(tileId);
-    checkIfGoalTile(tileId);
+    const previousTileId = player.occupiedTile;
+    const previousTile = document.getElementById(previousTileId);
+    if (previousTile) {
+        previousTile.classList.remove("occupied");
+    }
+    player.cartCoords = `${x},${y}`;
+    player.occupiedTile = getTile(player.cartCoords);
+    document.getElementById(player.occupiedTile).classList.add("occupied");
+    checkIfGoalTile(player.occupiedTile);
+    changeBlockedCorridors(previousTileId);
+    refreshButtons(player.occupiedTile);
 };
 
-// Function to set button states based on player position
-const setButtons = (x, y) => {
-    leftButton.disabled = x === 0;
-    rightButton.disabled = x === 2;
-    upButton.disabled = y === 0;
-    downButton.disabled = y === 2;
-};
-
-// Movement functions
-const changeXCoord = (newXCart) => {
-    let newXPx = xCartToPx(newXCart);
+// Functions to convert new cartesian co-ordinates into pixel values and apply them to player token
+const changeXCoord = newXCart => {
+    const newXPx = xCartToPx(newXCart);
     player.style.left = `${newXPx}px`;
     player.xPxCoord = newXPx;
     player.xCartCoord = newXCart;
 };
 
-const changeYCoord = (newYCart) => {
-    let newYPx = yCartToPx(newYCart);
+const changeYCoord = newYCart => {
+    const newYPx = yCartToPx(newYCart);
     player.style.top = `${newYPx}px`;
     player.yPxCoord = newYPx;
     player.yCartCoord = newYCart;
 };
 
-const goUp = () => {
-    const newYCoord = player.yCartCoord - 1;
-    changeYCoord(newYCoord);
+// Function to change cartesian coordinates depending on direction clicked
+const movePlayer = direction => {
+    switch (direction) {
+        case "Up":
+            player.yCartCoord--;
+            changeYCoord(player.yCartCoord);
+            break;
+        case "Left":
+            player.xCartCoord--;
+            changeXCoord(player.xCartCoord);
+            break;
+        case "Right":
+            player.xCartCoord++;
+            changeXCoord(player.xCartCoord);
+            break;
+        case "Down":
+            player.yCartCoord++;
+            changeYCoord(player.yCartCoord);
+            break;
+    }
 };
 
-const goDown = () => {
-    const newYCoord = player.yCartCoord + 1;
-    changeYCoord(newYCoord);
-};
-
-const goLeft = () => {
-    const newXCoord = player.xCartCoord - 1;
-    changeXCoord(newXCoord);
-};
-
-const goRight = () => {
-    const newXCoord = player.xCartCoord + 1;
-    changeXCoord(newXCoord);
-};
-
-
-//Define functions to randomise a goal and update its tile's style when selected and reached. 
+// Function to randomise a goal tile, update its style, remove its Id from ToDo and push it to Done
 const generateRandomGoal = () => {
     let goal;
     while (goal === player.occupiedTile || !ToDo.includes(goal)) {
-        let index = Math.floor(Math.random() * ToDo.length);
-        goal = ToDo[index]
+        let randomIndex = rand(ToDo.length);
+        goal = ToDo[randomIndex];
     }
-    return goal;
-}
-
-const hightlightGoal = (goal) => {
     const goalTile = document.getElementById(goal);
     goalTile.classList.add("goal");
-}
+};
 
-const getGoal = () => {
-    if (Done.length === 9){
-        alert("You Win!");
+// Function to check if the current tile is a goal tile, update its style. 
+const checkIfGoalTile = currentTile => {
+    const goalTile = document.querySelector(".goal").getAttribute("id");
+    if (currentTile !== goalTile) {
+        return;
     }
-    const goal = generateRandomGoal();
-    hightlightGoal(goal);
-    return goal;
-}
-
-const checkIfGoalTile = (tile) => {
-    if (tile !== goalTile) {
-        return
-    } else {
-        goalReached(tile);
-        goalTile = getGoal();
-    }
-}
-
-const goalReached = (tile) => {
-    const doneTile = document.getElementById(tile);
+    const doneTile = document.getElementById(currentTile);
     doneTile.classList.replace("goal", "done");
-    Done.push(tile);
-    const index = ToDo.indexOf(tile);
-    ToDo.splice(index, 1);
-}
+    const goalIdIndex = ToDo.indexOf(currentTile);
+    Done.push(ToDo.splice(goalIdIndex, 1));
+    // Check if all goals reached (game end)
+    if (Done.length === 9){
+        Buttons.disableAll();
+        document.getElementById("victory").style.visibility = "visible";
+    } else {
+        generateRandomGoal();
+    }
+};
 
-// Assign player element.
+// Function to block a random corridor adjacent to the previous tile and unblock another
+const changeBlockedCorridors = previousTile => {
+    // Sort corridors adjacent to the previous tile into blocked and blockable
+    const AdjacentCorridors = PossibleMovesByTile[previousTile][1];
+    const AllBlockedCorridors = Array.from(document.getElementsByClassName("blocked"), corridor => corridor.getAttribute("id"));
+    const AdjacentBlockedCorridors = AdjacentCorridors.filter(corridor => AllBlockedCorridors.includes(corridor));
+    const AdjacentBlockableCorridors = AdjacentCorridors.filter(corridor => !AllBlockedCorridors.includes(corridor));
+
+    // If there were adjacent blocked corridors, randomise which will be unblocked
+    if (AdjacentBlockedCorridors.length !== 0) {
+        let randomIndex = rand(AdjacentBlockedCorridors.length);
+        const corridorToUnblock = AdjacentBlockedCorridors[randomIndex];
+        document.getElementById(corridorToUnblock).classList.remove("blocked");
+    }
+
+    // Randomizse which Blockable tile is blocked
+    let randomIndex = rand(AdjacentBlockableCorridors.length);
+    const corridorToBlock = AdjacentBlockableCorridors[randomIndex];
+    document.getElementById(corridorToBlock).classList.add("blocked");
+};
+
+// Disable buttons depending on player location
+const refreshButtons = currentTile => {
+    Buttons.resetAll();
+    const AllBlockedCorridors = Array.from(document.getElementsByClassName("blocked"), corridor => corridor.getAttribute("id"));
+    const CurrentPossibleMoves = PossibleMovesByTile[currentTile][0];
+    const AdjacentCorridors = PossibleMovesByTile[currentTile][1];
+    const Directions = ['up', 'left', 'right', 'down'];
+
+    //Store number of directions disabled in a constant to determine if game lost
+    let blockedMoves = 0;
+    Directions.forEach(direction => {
+        if (!CurrentPossibleMoves.includes(direction)) {
+            Buttons[`${direction}Disable`]();
+            blockedMoves++;
+        }
+    });
+    AdjacentCorridors.forEach(corridor => {
+        if (AllBlockedCorridors.includes(corridor)) {
+            const index = AdjacentCorridors.indexOf(corridor);
+            const direction = CurrentPossibleMoves[index];
+            Buttons[`${direction}Disable`]();
+            blockedMoves++;
+        }
+    });
+
+    //Check game not won already
+    if (Done.length === 9) {
+        return;
+    //If not, loss
+    } else if (blockedMoves === 4) {
+        document.getElementById("loss").style.visibility = "visible";
+    }
+};
+
+// Assign player element
 const player = document.getElementById("player");
 
-// Initial setup
+// Player initialisation function
 player.start = () => {
     player.xPxCoord = getX();
     player.yPxCoord = getY();
     player.xCartCoord = xPxToCart(player.xPxCoord);
     player.yCartCoord = yPxToCart(player.yPxCoord);
-    player.cartCoords = getCoords(player.xPxCoord, player.yPxCoord);
+    player.cartCoords = `${player.xCartCoord},${player.yCartCoord}`;
     player.occupiedTile = getTile(player.cartCoords);
-    updateTiles(player.occupiedTile);
+    updateOccupiedTile(player.occupiedTile);
 };
 
-player.start();
-let goalTile;
-goalTile = getGoal();
+// Start game on start-button click
+document.addEventListener("DOMContentLoaded", () => {
+    const startButton = document.getElementById("enter");
+    startButton.addEventListener("click", () => {
+        const intro = document.getElementById("intro");
+        intro.remove();
+        player.start();
+        generateRandomGoal();
+        setButtonListeners();
+    });
+    // Restart game on button click on outcome panels
+    const victoryButton = document.getElementById("reset-win");
+    victoryButton.addEventListener("click", () => reset("victory"));
+    const deathButton = document.getElementById("reset-loss");
+    deathButton.addEventListener("click", () => reset("death"));
+});
+
+// Reset function after game ended
+const reset = outcome => {
+    if (outcome === "victory") {
+        document.getElementById("victory").style.visibility = "hidden";
+    }
+    if (outcome === "death") {
+        document.getElementById("loss").style.visibility = "hidden";
+    }
+
+    ToDo.length = 0;
+    ToDo.push('sp0', 'sp1', 'sp2', 'sp3', 'sp4', 'sp5', 'sp6', 'sp7', 'sp8');
+    Done.length = 0;
+    
+    player.xPxCoord = 72;
+    player.style.left = `${player.xPxCoord}px`;
+    player.xCartCoord = xPxToCart(player.xPxCoord);
+    player.yPxCoord = 92;
+    player.style.top = `${player.yPxCoord}px`;
+    player.yCartCoord = yPxToCart(player.yPxCoord);
+    player.cartCoords = `${player.xCartCoord},${player.yCartCoord}`;
+    player.occupiedTile = getTile(player.cartCoords);
+
+    for (let i = 0; i <= 8; i++){
+        document.getElementById(`sp${i}`).setAttribute("class", "space");
+    }
+    for (let i = 0; i <= 11; i++){
+        document.getElementById(`cor${i}`).setAttribute("class", "corridor");
+    }
+
+    updateOccupiedTile(player.occupiedTile);
+    generateRandomGoal();
+    Buttons.resetAll();
+};
